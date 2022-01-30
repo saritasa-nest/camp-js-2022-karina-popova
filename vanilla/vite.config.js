f74dd01bbc0ecbb58442a010687a8cd2fe7c3d0a
karbik-creator <car1na.p@yandex.ru>
2022-01-30 23:49:37 +0500

    create form login and form register

---
+++ vanilla/vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root,
+  base:'./',
+  plugins: [],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: [
+        resolve(root, 'index.html')
+      ]
+    }
+  }
+});
