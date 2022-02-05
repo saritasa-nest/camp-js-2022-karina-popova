801137c988a6cf30ce57eb36f2556ef2fcc2cc44
karbik-creator <car1na.p@yandex.ru>
2022-02-05 14:19:30 +0500

    add render films #JS19-259

---
+++ vanilla/src/films/getFilms.ts
@@ -0,0 +1,30 @@
+import { CollectionReference, Query } from 'firebase/firestore';
+
+import { getDocsFilms } from '../ts/getDocsFilms';
+
+import { Film } from './film';
+
+/**
+ * Get array films.
+ * @param collectionFilmsReference A CollectionReference object can be used for adding documents,
+ *  getting document references, and querying for documents.
+ * @returns {Promise<Film[]>} Promise<Film[]>.
+ */
+export async function getFilms(collectionFilmsReference: CollectionReference | Query): Promise<Film[]> {
+  const films: Film[] = [];
+  const querySnapshot = await getDocsFilms(collectionFilmsReference);
+  querySnapshot.forEach(doc => {
+    const item: Film = {
+      id: doc.id,
+      created: new Date(doc.data().fields.created).toLocaleDateString('en-GB'),
+      director: doc.data().fields.director,
+      edited: new Date(doc.data().fields.edited),
+      openingCrawl: doc.data().fields.episode_id,
+      producer: doc.data().fields.producer,
+      releaseDate: doc.data().fields.release_date,
+      title: doc.data().fields.title,
+    };
+    films.push(item);
+  });
+  return films;
+}
---
+++ vanilla/src/films/renderFilms.ts
@@ -0,0 +1,38 @@
+import {
+  DocumentData,
+  QueryDocumentSnapshot
+} from 'firebase/firestore';
+
+import { getPatternFilms } from './getPatternFilms';
+import { getDocsFilms } from '../ts/getDocsFilms';
+import { getFilms } from './getFilms';
+import { PageArrow } from '../enum/enum';
+import { getQueryLimit } from '../ts/getQueryLimit';
+
+export const pageLimit = 3;
+export let lastDocFilm: QueryDocumentSnapshot<DocumentData> | null = null;
+const container = document.querySelector<Element>('tbody');
+/**
+ * Display films.
+ * @param {Settings} settings Films container and collection reference.
+ * @param PageArrow
+ */
+export async function renderFilms(pageArrow?: PageArrow): Promise<void> {
+  const collectionLimitFilmsReference = getQueryLimit(pageArrow);
+  const docsFilms = await getDocsFilms(collectionLimitFilmsReference);
+  const films = await getFilms(collectionLimitFilmsReference);
+
+  if (container) {
+    container.innerHTML = '';
+  }
+
+  container?.insertAdjacentHTML(
+    'afterbegin',
+    getPatternFilms(films.slice(0, pageLimit))
+  );
+
+  if (lastDocFilm !== undefined) {
+    lastDocFilm = docsFilms.docs[pageLimit] ?? null;
+  }
+}
+
